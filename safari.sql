60ed7b61602d3052d97a6acf62c853ba2dde6b78
Karen Yip <karenyip1999@gmail.com>
2024-02-21 16:27:31 +0000

    Created 4 tables

--- safari.sql
+++ safari.sql
@@ -3,10 +3,17 @@ DROP TABLE IF EXISTS animal;
 DROP TABLE IF EXISTS staff;
 DROP TABLE IF EXISTS enclosure;
 
-CREATE TABLE assignment(
+CREATE TABLE enclosure(
     id SERIAL PRIMARY KEY,
-    employee_id int REFERENCES staff(id),
-    enclosure_id int REFERENCES enclosure(id) 
+    name VARCHAR(255),
+    capacity int,
+    closedForMaintenance BOOLEAN
+);
+
+CREATE TABLE staff(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    employeeNumber int
 );
 
 CREATE TABLE animal(
@@ -17,15 +24,9 @@ CREATE TABLE animal(
     enclosure_id int REFERENCES enclosure(id)
 );
 
-CREATE TABLE staff(
+CREATE TABLE assignment(
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    employeeNumber int
+    employee_id int REFERENCES staff(id),
+    enclosure_id int REFERENCES enclosure(id) 
 );
 
-CREATE TABLE enclosure(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    capacity int,
-    closedForMaintenance BOOLEAN
-);
